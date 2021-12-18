44e65eaebada63be22551689f09ae4120f0ae530
MfsTeller <i1211435.kosen@gmail.com>
2021-12-18 09:41:06 +0900

    modify error handling

--- certs/create.sh
+++ certs/create.sh
@@ -2,19 +2,23 @@
 
 set -e
 
-echo "AZURITE_CONTROLLER: INFO: cloning mkcert repository started"
+function logger_info () {
+    echo "AZURITE_CONTROLLER: INFO: $1"
+}
+
+logger_info "cloning mkcert repository started"
 git clone https://github.com/FiloSottile/mkcert
-echo "AZURITE_CONTROLLER: INFO: cloning mkcert repository completed"
+logger_info "cloning mkcert repository completed"
 
-echo "AZURITE_CONTROLLER: INFO: building mkcert started"
+logger_info "building mkcert started"
 cd mkcert && go build -ldflags "-X main.Version=$(git describe --tags)" && cd ../
-echo "AZURITE_CONTROLLER: INFO: building mkcert completed"
+logger_info "building mkcert completed"
 
-echo "AZURITE_CONTROLLER: INFO: creating locally-trusted development certificates started"
+logger_info "creating locally-trusted development certificates started"
 ./mkcert/mkcert -install
 ./mkcert/mkcert 127.0.0.1
-echo "AZURITE_CONTROLLER: INFO: creating locally-trusted development certificates completed"
+logger_info "creating locally-trusted development certificates completed"
 
-echo "AZURITE_CONTROLLER: INFO: deleting mkcert repository started"
+logger_info "deleting mkcert repository started"
 rm -rf mkcert
-echo "AZURITE_CONTROLLER: INFO: deleting mkcert repository completed"
+logger_info "deleting mkcert repository completed"
